87f3a54f328106037f7145e963717e8fcd1d4acd
enaam0 <115560426+enaam0@users.noreply.github.com>
2022-11-23 21:56:11 +0100

    Add files via upload

---
+++ ft_printf/ft_putnbr_p.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr_p.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: enaam <enaam@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/21 16:06:36 by enaam             #+#    #+#             */
+/*   Updated: 2022/11/23 17:48:12 by enaam            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+void	ft_putnbr_p(unsigned long long nb, int *count)
+{
+	char	*hx;
+
+	hx = "0123456789abcdef";
+	if (nb >= 0 && nb < 16)
+	{
+		nb = hx[nb];
+		ft_putchar(nb, count);
+	}
+	else
+	{
+		ft_putnbr_p(nb / 16, count);
+		ft_putnbr_p(nb % 16, count);
+	}
+}
